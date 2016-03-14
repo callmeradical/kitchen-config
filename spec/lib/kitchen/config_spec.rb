a49e2498f7cf972591a138d9d8504b9a4010d992
Lars Cromley <lars@paniksrvr.info>
2016-03-14 19:07:32 -0400

    externalize config.yml to environment variable

--- spec/lib/kitchen/config_spec.rb
+++ spec/lib/kitchen/config_spec.rb
@@ -1,53 +1,60 @@
 require 'spec_helper'
 
 RSpec.describe Kitchen::Config do
-  mimic_config = {
-    key_id: 'super_secret_key',
-    tester_ami: 'ami-666',
-    bare_ami: 'ami-999',
-    security_group_ids: ['sg-123456']
-  }
+  context 'A Valid Kitchen Config' do
+    mimic_config = {
+      key_id: 'super_secret_key',
+      tester_ami: 'ami-666',
+      bare_ami: 'ami-999',
+      security_group_ids: ['sg-123456']
+    }
 
-  cb_name = 'FartBook'
-  user = 'Derp'
-  os = 'DerpOS'
+    cb_name = 'FartBook'
+    user = 'Derp'
+    os = 'DerpOS'
 
-  let(:subj) { Kitchen::Config }
-  subject { subj }
-  before :each do
-    subj.instance_variable_set('@config', mimic_config)
-  end
-
-  describe '#tags' do
-    let(:method) { subj.tags(cb_name, user) }
-    it { expect(subj).to respond_to(:tags) }
-    it { expect(method).to match /Name: \"Chef Tester\"/ }
-    it { expect(method).to match /Apps: \"#{cb_name} cookbook test\"/ }
-  end
-
-  describe 'values returned for config' do
-    it { expect(subj.tester_ami).to eq('ami-666') }
-    it { expect(subj.bare_ami).to eq('ami-999') }
-    it { expect(subj.key_id).to eq('super_secret_key') }
-  end
-
-  describe 'deprecated methods' do
-    it 'presents a warning to the user' do
-      expect(Kernel).to receive(:warn)
-      subj.security_group_ids_yml
+    let(:subj) { Kitchen::Config }
+    subject { subj }
+    before :each do
+      config = File.expand_path(__FILE__)
+      ENV['KITCHEN_CONFIG'] = "#{config}/config.yml"
+      subj.instance_variable_set('@config', mimic_config)
     end
-    it 'calls the new method for the user' do
-      expect(subj).to receive(:security_group_ids)
-      subj.security_group_ids_yml
+
+    describe '#tags' do
+      let(:method) { subj.tags(cb_name, user) }
+      it { expect(subj).to respond_to(:tags) }
+      it { expect(method).to match /Name: \"Chef Tester\"/ }
+      it { expect(method).to match /Apps: \"#{cb_name} cookbook test\"/ }
+    end
+
+    describe '#load_config' do
+      let(:method) { subj.load_config(ENV['KITCHEN_CONFIG']) }
+      it { expect(subj).to respond_to(:load_config) }
+    end
+
+    describe 'values returned for config' do
+      it { expect(subj.tester_ami).to eq('ami-666') }
+      it { expect(subj.bare_ami).to eq('ami-999') }
+      it { expect(subj.key_id).to eq('super_secret_key') }
+    end
+
+    describe 'if key is not present in config' do
+      it 'raises an exception' do
+        expect { subj.foo }.to raise_error /Undefined method/
+      end
+      it 'presents the user with available keys' do
+        expect { subj.bar }.to raise_error /:key_id, :tester_ami, :bare_ami/
+      end
     end
   end
 
-  describe 'if key is not present in config' do
-    it 'raises an exception' do
-      expect { subj.foo }.to raise_error /Undefined method/
-    end
-    it 'presents the user with available keys' do
-      expect { subj.bar }.to raise_error /:key_id, :tester_ami, :bare_ami/
+  context 'An INVALID Kitchen Config' do
+    let(:subj) { Kitchen::Config }
+    describe 'if KITCHEN_CONFIG is not valid' do
+      it 'it raises an exception' do
+        expect { subj.load_config(nil) }.to raise_error /ArguementError/
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 Dir['./lib/**/*.rb'].reverse_each { |file| require file }
-
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   RSpec::Mocks.configuration.verify_partial_doubles = true
